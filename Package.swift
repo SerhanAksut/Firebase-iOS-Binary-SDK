a7adba322a60a500803a7765c4deb49722a68460
Serhan Aksüt <serhanaksut@gmail.com>
2022-04-02 21:59:33 +0200

    PromisesObjc xcframework has been added.

--- Package.swift
+++ Package.swift
@@ -5,68 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "FirebaseiOSSDK",
-    platforms: [
-        .iOS(.v13),
-        .macOS(.v10_15)
-    ],
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
-        .library(
-            name: "FirebaseCore",
-            targets: ["FirebaseCore"]
-        ),
-        .library(
-            name: "FirebaseFirestore",
-            targets: ["FirebaseFirestore"]
-        ),
-        .library(
-            name: "FirebaseInstallations",
-            targets: ["FirebaseInstallations"]
-        ),
-        .library(
-            name: "FirebaseMessaging",
-            targets: ["FirebaseMessaging"]
-        ),
-        .library(
-            name: "GoogleDataTransport",
-            targets: ["GoogleDataTransport"]
-        ),
-        .library(
-            name: "GoogleUtilities",
-            targets: ["GoogleUtilities"]
-        ),
-        .library(
-            name: "nanopb",
-            targets: ["nanopb"]
-        ),
+        .library(name: "FirebaseCore", targets: ["FirebaseCore"]),
+        .library(name: "FirebaseFirestore", targets: ["FirebaseFirestore"]),
+        .library(name: "FirebaseInstallations", targets: ["FirebaseInstallations"]),
+        .library(name: "FirebaseMessaging", targets: ["FirebaseMessaging"]),
+        .library(name: "GoogleDataTransport", targets: ["GoogleDataTransport"]),
+        .library(name: "GoogleUtilities", targets: ["GoogleUtilities"]),
+        .library(name: "nanopb", targets: ["nanopb"]),
+        .library(name: "PromisesObjC", targets: ["PromisesObjC"])
     ],
     targets: [
-        .binaryTarget(
-            name: "FirebaseCore",
-            path: "FirebaseCore.xcframework"
-        ),
-        .binaryTarget(
-            name: "FirebaseFirestore",
-            path: "FirebaseFirestore.xcframework"
-        ),
-        .binaryTarget(
-            name: "FirebaseInstallations",
-            path: "FirebaseInstallations.xcframework"
-        ),
-        .binaryTarget(
-            name: "FirebaseMessaging",
-            path: "FirebaseMessaging.xcframework"
-        ),
-        .binaryTarget(
-            name: "GoogleDataTransport",
-            path: "GoogleDataTransport.xcframework"
-        ),
-        .binaryTarget(
-            name: "GoogleUtilities",
-            path: "GoogleUtilities.xcframework"
-        ),
-        .binaryTarget(
-            name: "nanopb",
-            path: "nanopb.xcframework"
-        )
+        .binaryTarget(name: "FirebaseCore", path: "FirebaseCore.xcframework"),
+        .binaryTarget(name: "FirebaseFirestore", path: "FirebaseFirestore.xcframework"),
+        .binaryTarget(name: "FirebaseInstallations", path: "FirebaseInstallations.xcframework"),
+        .binaryTarget(name: "FirebaseMessaging", path: "FirebaseMessaging.xcframework"),
+        .binaryTarget(name: "GoogleDataTransport", path: "GoogleDataTransport.xcframework"),
+        .binaryTarget(name: "GoogleUtilities", path: "GoogleUtilities.xcframework"),
+        .binaryTarget(name: "nanopb", path: "nanopb.xcframework"),
+        .binaryTarget(name: "PromisesObjC", path: "PromisesObjC.xcframework")
     ]
 )
